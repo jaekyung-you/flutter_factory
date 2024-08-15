336bd0424ceaf67efee156e815cf345a700c6da5
jaekyungyou <youjk96@gmail.com>
2024-08-15 14:49:42 +0900

    pg, 소셜 로그인, 지도 폴더 추가

--- lib/routes/factory_path.dart
+++ lib/routes/factory_path.dart
@@ -1,3 +1,3 @@
 class FlutterPath {
-
+  static const HOME = '/';
 }
--- lib/routes/factory_routes.dart
+++ lib/routes/factory_routes.dart
@@ -1,3 +1,18 @@
-class FlutterRoutes {
+import 'package:get/get.dart';
 
+import '../main.dart';
+import 'factory_path.dart';
+
+class FlutterRoutes {
+  FlutterRoutes._privateConstructor();
+
+  static final _instance = FlutterRoutes._privateConstructor();
+
+  factory FlutterRoutes() {
+    return _instance;
+  }
+
+  static final routes = [
+    GetPage(name: FlutterPath.HOME, page: () => const MyHomePage()),
+  ];
 }
