30445477c5bc799a7a727b16a11a15b97824fba4
jaekyungyou <youjk96@gmail.com>
2024-08-15 14:32:31 +0900

    폴더구조 생성, 라이브러리 추가

---
+++ lib/api/api_base.dart
@@ -0,0 +1,11 @@
+class DioApiBase {
+
+  DioApiBase._privateConstructor();
+
+  static final _instance = DioApiBase._privateConstructor();
+
+  factory DioApiBase() {
+    return _instance;
+  }
+
+}
---
+++ lib/config/app_config.dart
@@ -0,0 +1,19 @@
+class Theme {
+  // vivid red
+
+  // vivid blue
+
+  // vivid green
+
+  // vivid purple
+
+  // black & white
+
+  // pastel pink
+
+  // pastel blue
+
+  // pastel purple
+
+
+}
