61b86dceb4ad796fbd8e2c35e2a88d3bfec75893
jaekyungyou <youjk96@gmail.com>
2024-08-17 15:38:56 +0900

    탭바 수정

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
         fontFamily: "Pretendard",
       ),
@@ -42,44 +42,81 @@ class _MyHomePageState extends State<MyHomePage> {
     mainController = Get.put(MainController());
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(''),
-      ),
-      body: Obx(() => Container(
-            child: (() {
-              switch (mainController.currentTabIndex.value) {
-                case 0:
-                  return const HomeScreen();
+  static const List<Widget> screens = [
+    HomeScreen(),
+    HomeScreen(),
+    MapScreen(),
+    HomeScreen(), // todo: 앱 시동시 자동로그인이면 MyPageScreen으로 전환
+    HomeScreen(),
+  ];
 
-                case 1:
-                case 2:
-                  return const MapScreen();
-                case 3:
-                case 4:
-              }
-            })(),
-          )),
-      bottomNavigationBar: mainBottomNav(),
-    );
+  int selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 
-  Widget mainBottomNav() {
-    return Obx(() {
-      return BottomNavigationBar(
-        currentIndex: mainController.currentTabIndex.value,
-        onTap: mainController.setBottomTab,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: '메인', backgroundColor: Colors.purple),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: '기능1', backgroundColor: Colors.purple),
+  @override
+  Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    //     title: Text(''),
+    //   ),
+    //   body: Obx(() => Container(
+    //         child: (() {
+    //           switch (mainController.currentTabIndex.value) {
+    //             case 0:
+    //               return const HomeScreen();
+    //
+    //             case 1:
+    //             case 2:
+    //               return const MapScreen();
+    //             case 3:
+    //             case 4:
+    //           }
+    //         })(),
+    //       )),
+    //   bottomNavigationBar: mainBottomNav(),
+    // );
+
+    return Scaffold(
+      body: screens.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.blue,
+        selectedIconTheme: const IconThemeData(color: Colors.blue),
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '메인'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: '기능1'),
           BottomNavigationBarItem(icon: Icon(Icons.map), label: '기능2'),
           BottomNavigationBarItem(icon: Icon(Icons.question_answer), label: '기능3'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'MY'),
         ],
-      );
-    });
+        currentIndex: selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
+
+// Widget mainBottomNav() {
+//   return Obx(() {
+//     return BottomNavigationBar(
+//       currentIndex: mainController.currentTabIndex.value,
+//       onTap: mainController.setBottomTab,
+//       items: [
+//         BottomNavigationBarItem(icon: Icon(Icons.home), label: '메인', backgroundColor: Colors.purple),
+//         BottomNavigationBarItem(icon: Icon(Icons.list), label: '기능1', backgroundColor: Colors.purple),
+//         BottomNavigationBarItem(icon: Icon(Icons.map), label: '기능2'),
+//         BottomNavigationBarItem(icon: Icon(Icons.question_answer), label: '기능3'),
+//         BottomNavigationBarItem(icon: Icon(Icons.person), label: 'MY'),
+//       ],
+//     );
+//   });
+// }
